caac4668cce0e23ef564aa906eaf9a6d4c8c45c4
Manashi Paul <Manashi Paul>
2017-08-01 12:35:16 -0400

    configure git

--- worksheet-1.sh
+++ worksheet-1.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 ## Configure git
-git config --global user.name ...
-git config --global user.email ...
+git config --global user.name manashipaul
+git config --global user.email paul.manashi117@gmail.com
 
 ## Change the "origin" remote URL
-git remote set-url origin ...
+#git remote set-url origin ...
 
 ## Set the SESYNC-CI repository upstream and pull updates
-git remote add upstream https://github.com/sesync-ci/handouts.git
-git pull upstream master
+#git remote add upstream https://github.com/sesync-ci/handouts.git
+#git pull upstream master
